b109944633e8eb5685fd588b1016578a8db6b8c8
Miguel Uy <yuleugim@gmail.com>
2016-08-24 00:32:31 -0400

    Added icons, breakpoints, and media queries
    Added a scss mixin for a $breakpoints map, thanks to the help of thesassway.com for showing how it's done. Icons to the pentesting section was added. Media queries to better support the new content was added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
   $('.bg-holder').parallaxScroll({
-  friction: 0.3
-});
+    friction: 0.2
+  });
+
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    
+    if (target.length) {
+      event.preventDefault();
+      
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
 });

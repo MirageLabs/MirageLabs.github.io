3e096f0200576e12377c96dfdfeb67f86b7e0c58
Miguel Uy <yuleugim@gmail.com>
2016-08-28 00:59:22 -0400

    Began work on refactoring to materialize
    Sorry for the mess. Will try to commit more often.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
-  $('.bg-holder').parallaxScroll({
-    friction: 0.2
-   });
-
-
-  $('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    
-    if (target.length) {
-      event.preventDefault();
-      
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
+   $(".button-collapse").sideNav();
+   $('.carousel.carousel-slider').carousel({full_width: true});
+   $('.parallax').parallax();
 });

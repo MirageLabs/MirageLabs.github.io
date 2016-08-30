2119cb4e8855e5df631789e847ad591472eea5f9
Miguel Uy <yuleugim@gmail.com>
2016-08-30 00:35:41 -0400

    Added jquery ui for transitions
    Also added Managed Security section since I forgot about it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-   $(".button-collapse").sideNav();
-   $('.carousel.carousel-slider').carousel({full_width: true});
-   $('.parallax').parallax();
+  $(".button-collapse").sideNav();
+  $('.carousel.carousel-slider').carousel({full_width: true});
+  $('.parallax').parallax();
+
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, "easeInOutQuart", function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+
 });
